744dbdd234a7c1997fe315b085cbfe3ce2b343e1
izexi <163058785+izexi7@users.noreply.github.com>
2024-03-12 20:21:59 +0000

    update(config.js)

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: 'MTIxNjQwMjU0NzU5MDU2MTg5Mg.Gi1Kop.Rnf8JYiHqjSFJND9K8ZvVEXM-GTyaATldlMXxI',
-    clientid: '1216402547590561892'
+    token: 'BOT_TOKEN',
+    clientid: 'BOT_CLIENT_ID'
 }
